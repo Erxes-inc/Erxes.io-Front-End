008862007ab0b81cc73124503f5748b1fb6db6c9
Chingun Erdene-Ochir <33973591+Jhingun1@users.noreply.github.com>
2020-04-20 20:39:25 +1000

    Add files via upload

---
+++ mixpanel.js
@@ -0,0 +1,12 @@
+var currentUrl={"url":window.location.href,};function mix__signUp(){mixpanel.track("Create a New Organization - Sign up",currentUrl);}
+function mix__confirm(){mixpanel.track("Confirm Email - Confirm",currentUrl);}
+function mix__create(){mixpanel.track("Create an Organization - Create",currentUrl);}
+function mix__getStarted(){mixpanel.track("Header - Get Started",currentUrl);}
+function mix__tryFree(){mixpanel.track("Homepage - Try For Free",currentUrl);}
+function mix__launchDemo(){mixpanel.track("Homepage - Launch Demo",currentUrl);}
+function mix__getErxes(){mixpanel.track("Homepage Footer - Get erxes Free",currentUrl);}
+function mix__tryIt(){mixpanel.track("Homepage Footer - Try It Free",currentUrl);}
+function mix__installServer(){mixpanel.track("Homepage Footer - Install Server",currentUrl);}
+function mix__getFree(){mixpanel.track("Pricing Page - Create Free Account",currentUrl);}
+function mix__contactSales(){mixpanel.track("Pricing Page - Contact To Sales",currentUrl);}
+function mix__getInTouch(){mixpanel.track("Pricing Page - Get In Touch",currentUrl);}
